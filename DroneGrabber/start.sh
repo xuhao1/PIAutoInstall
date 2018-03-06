b3a0c4566576d9d360e97e71a9543d5a8dc490a0
xuhao1 <xuhao3e8@gmail.com>
2018-03-06 15:17:42 +0800

    Mv to submodule

--- DroneGrabber/start.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
- #sudo xvfb-run --server-args='-screen 0, 1024x768x24' /home/pi/PIAutoInstall/nwjs-v0.27.6-linux-arm/nw /home/pi/PIAutoInstall/DroneGrabber
-sudo xvfb-run --server-args='-screen 0, 1024x768x24' /home/pi/PIAutoInstall/nwjs-sdk-v0.23.7-linux-arm/nw /home/pi/PIAutoInstall/DroneGrabber
