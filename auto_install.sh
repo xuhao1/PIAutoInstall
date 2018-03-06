e80dd96d1daf93b87b7427bbc9ae36c957cccc40
xuhao1 <xuhao3e8@gmail.com>
2018-03-06 15:32:56 +0800

    Fix git url

--- auto_install.sh
+++ auto_install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+echo "Starting setup PI"
 sudo apt-get update
+echo "Install wget xvfb"
 sudo apt-get install wget xvfb
+echo "Install python-lxml"
 sudo apt-get install python-lxml
+echo "Install mavproxy"
 sudo -H pip install mavproxy
 #Install nwjs
+echo "Install node"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-git clone git@github.com:xuhao1/PIAutoInstall.git
+echo "Download code"
+git clone https://github.com/xuhao1/PIAutoInstall.git
 cd PIAutoInstall
+echo "Setup-codes"
 bash configure_pi.sh
+echo "Setup Finish, why not reboot and have a try?"
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Introduction
+This project is auto install DroneRTC system to a Raspberry PI
+## Usage
+
+Login to your Raspberry PI with ssh and under home directory
+
+>curl -sL https://raw.githubusercontent.com/xuhao1/PIAutoInstall/master/auto_install.sh | sudo -E bash -
+
+After code runned, plz restart. That is all fun!
